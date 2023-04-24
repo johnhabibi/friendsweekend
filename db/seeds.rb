f051acaf999aa460dbb2d7db74d82f9422c5e54f
John Habibi <johnhabibi@gmail.com>
2023-04-24 15:45:54 -0500

    change countdown target time (#2)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 
-
-event_dates = EventDate.create(date: "2023-06-03 11:10:00", location: "Camp Hill, Pennsylvania")
+# The party doesn't officially start until the last person gets there
+event_dates = EventDate.create(date: "2023-06-03 18:45:00", location: "Camp Hill, Pennsylvania")
 travel_iteneraries = TravelItenerary.create(traveler: "John Habibi", provider: "Southwest Airlines", departure: "2023-06-03 07:00:00", arrival: "2023-06-03 11:10:00", identifier: "360", arrival_code: "PHL")
